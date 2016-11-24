996c70155615f307718745c6b3d52b4cf5377c1a
Toth Szilard <szilardtoth94@gmail.com>
2016-11-24 16:52:18 +0200

    candidateinfo activity design 90%

---
+++ TechInterview/app/src/main/java/com/halcyonmobile/techinterview/CandidateInfo.java
@@ -0,0 +1,13 @@
+package com.halcyonmobile.techinterview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CandidateInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidate_info);
+    }
+}
