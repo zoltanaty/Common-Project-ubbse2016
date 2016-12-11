79e7396d544006873de2dfd6e7a03b48a2f159a0
Mate Zoltan <zoltanaty@gmail.com>
2016-12-11 20:12:48 +0200

    Changes on Android.

--- TechInterview/app/src/main/java/com/halcyonmobile/techinterview/src/activities/QuestionareActivity.java
+++ TechInterview/app/src/main/java/com/halcyonmobile/techinterview/src/activities/QuestionareActivity.java
@@ -1,16 +1,22 @@
 package com.halcyonmobile.techinterview.src.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.halcyonmobile.techinterview.R;
 
-public class QuestionareActivity extends AppCompatActivity {
+public class QuestionareActivity extends FragmentActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionare);
+
+
 
     }
 }
--- TechInterview/app/src/main/java/com/halcyonmobile/techinterview/src/activities/WelcomeActivity.java
+++ TechInterview/app/src/main/java/com/halcyonmobile/techinterview/src/activities/WelcomeActivity.java
@@ -26,7 +26,7 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(WelcomeActivity.this, QuestionareActivity.class);
+                Intent intent = new Intent(WelcomeActivity.this,QuestionareActivity.class);
                 WelcomeActivity.this.startActivity(intent);
             }
         });
