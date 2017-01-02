7b43af5567cde4dbf6594baaf3621bc2af87522c
András Fodor <muandrs@gmail.com>
2017-01-02 15:18:24 +0200

    Foregin key: answer -> question

--- db_dump/structure_updates.sql
+++ db_dump/structure_updates.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `result` ADD `thinking_time` INT NOT NULL AFTER `date`;
 ALTER TABLE `result` ADD `is_correct` BIT(1) NOT NULL AFTER `thinking_time`;
+ALTER TABLE `answer` ADD CONSTRAINT `answerFKid_question` FOREIGN KEY (`id_question`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
